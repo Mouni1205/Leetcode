3444112347a15f1c31ed94f8af88476d6dd9be94
Mouni1205 <65585689+Mouni1205@users.noreply.github.com>
2025-05-18 11:52:45 -0400

    Time: 2 ms (50.14%), Space: 48.1 MB (29.14%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,25 +1,27 @@
 class Solution {
-    public void dfs(int[][] isConnected, int i, boolean[] visited) {
-        visited[i] = true;
-        for (int j = 0; j < isConnected.length; j++) {
-            if (isConnected[i][j] == 1 && !visited[j]) {
-                dfs(isConnected, j, visited);
-            }
-        }
-    }
-
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        boolean[] visited = new boolean[n];
-        int provinces = 0;
+        boolean visited[] = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        int result = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(isConnected, i, visited);
-                provinces++;
+        for (int j = 0; j < n; j++) {
+            if (!visited[j]) {
+                queue.offer(j);  // Start BFS from the current unvisited city
+                visited[j] = true;  // Mark as visited
+                result++;  // Increment province count
+
+                while (!queue.isEmpty()) {
+                    int crnt = queue.poll();
+                    for (int i = 0; i < n; i++) {
+                        if (isConnected[crnt][i] == 1 && !visited[i]) {
+                            queue.offer(i);
+                            visited[i] = true;
+                        }
+                    }
+                }
             }
         }
-
-        return provinces;
+        return result;
     }
 }
