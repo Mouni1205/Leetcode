a2b21df32183ffd966c7a2404fc1a74a699d6ac0
Mouni1205 <65585689+Mouni1205@users.noreply.github.com>
2025-05-25 11:51:10 -0400

    Time: 45 ms (70.67%), Space: 114 MB (59.61%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,63 +1,65 @@
-class Node {
+class Node{
     int key,value;
-    Node prev;
-    Node next;
-    public Node(int key,int value){
+    Node prev,next;
+    Node(int key,int value){
         this.key = key;
         this.value = value;
     }
 }
 class LRUCache {
-    HashMap<Integer , Node> map;
-    Node head,tail;
-    int capacity;
+    HashMap<Integer,Node> map;
+    int capacity = 0;
+    Node dummyHead,dummyTail;
     public LRUCache(int capacity) {
-        //initialize capacity,map and create dummy head and tail 
-        map = new HashMap<>();
         this.capacity = capacity;
-        head = new Node(-1,-1);
-        tail = new Node(-1,-1);
-        head.next = tail;
-        tail.prev = head;
+        map = new HashMap<>();
+        dummyHead = new Node(-1,-1);
+        dummyTail = new Node(-1,-1);
+        dummyHead.next = dummyTail;
+        dummyTail.prev = dummyHead;
     }
-    //we have to update both prev and next nodes of crnt
-    public  void remove(Node crnt){
-        crnt.prev.next = crnt.next;
-        crnt.next.prev = crnt.prev;
-    }
-    public  void insert(Node crnt){
-        crnt.next = head.next;
-        crnt.next.prev = crnt;
-        head.next = crnt;
-        crnt.prev = head;
-    }
+    
     public int get(int key) {
-        //if the map contains key we have to first remove and then insert since we accessed it recently it goes to the front of the linkedlist
-        //since the order of the linked list goes from recently accessed keys to the least accessed keys.
+        //we have to do two things return value from map and then if the key exists we want to remove the node and then add it to the front
         if(map.containsKey(key)){
             Node crnt = map.get(key);
             remove(crnt);
-            insert(crnt);
+            addFirst(crnt);
             return crnt.value;
         }
         return -1;
     }
     
     public void put(int key, int value) {
-        //if the map contains key then remove and then insert at the beginning
         if(map.containsKey(key)){
-            remove(map.get(key));
-            map.remove(key);
-        }  
-        //if the capacity is full then remove the least accessed which is the tail.prev of the DLL; tail is a dummy node and also from map
-        if(map.size()==capacity){
-            map.remove(tail.prev.key);
-            remove(tail.prev);
+            Node crnt = map.get(key);
+            crnt.value = value;
+            map.put(key,crnt);
+            remove(crnt);
+            addFirst(crnt);
+        }else{
+            Node crnt = new Node(key,value);
+            map.put(key,crnt);
+            if(map.size()>capacity){
+                map.remove(dummyTail.prev.key);
+                remove(dummyTail.prev);
+            }
+            addFirst(crnt);
         }
-        //insert into DLL and also into the map
-        Node crnt = new Node(key,value);
-        insert(crnt);
-        map.put(key,crnt);
+    }
+
+    public void remove(Node node){
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        node.prev = null;
+        node.next = null;
+    }
+
+    public void addFirst(Node node){
+        node.next = dummyHead.next;
+        node.prev = dummyHead;
+        dummyHead.next = node;
+        node.next.prev = node;
     }
 }
 
