ab0aa8b82ec00c6f87841c6f6113093bd00db089
Mouni1205 <65585689+Mouni1205@users.noreply.github.com>
2025-06-25 20:14:46 -0500

    Time: 18 ms (24.75%), Space: 49.4 MB (7.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,13 @@
-//thought process: Anagrams are something where two strings will have the same freq of letters based on this if we could have data structure 
-//which stores the unique keys which represents the string in a numerical way and keep on adding the strings to that group which have the 
-//same numerical representation of the string
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,ArrayList<String>> map = new HashMap<>();
         for(String str:strs){
             int freq[]=new int[26];
-            for(char ch:str.toCharArray())
-                freq[ch-'a']++;
+            for(char ch:str.toCharArray()) freq[ch-'a']++;
             String key = Arrays.toString(freq);
             if(!map.containsKey(key)) map.put(key,new ArrayList<>());
-            map.get(key).add(str);   
+            map.get(key).add(str);
         }
-        return new ArrayList<>(map.values());
+       return new ArrayList<>(map.values());
     }
 }
