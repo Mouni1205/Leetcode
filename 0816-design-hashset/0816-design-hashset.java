a98cb71479503c470237c3c022e310d34b24a5e2
Mouni1205 <65585689+Mouni1205@users.noreply.github.com>
2025-07-14 17:52:43 -0500

    Time: 16 ms (38.31%), Space: 46 MB (99.53%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.java
+++ 0816-design-hashset/0816-design-hashset.java
@@ -1,26 +1,53 @@
-class MyHashSet {
-    HashSet<Integer> set;
+public class MyHashSet {
+
+    private static class ListNode {
+        int key;
+        ListNode next;
+
+        ListNode(int key) {
+            this.key = key;
+        }
+    }
+
+    private final ListNode[] set;
+
     public MyHashSet() {
-        set = new HashSet();
+        set = new ListNode[10000];
+        for (int i = 0; i < set.length; i++) {
+            set[i] = new ListNode(0);
+        }
     }
-    
+
     public void add(int key) {
-        set.add(key);
+        ListNode cur = set[key % set.length];
+        while (cur.next != null) {
+            if (cur.next.key == key) { //to check the duplicates
+                return;
+            }
+            cur = cur.next;
+        }
+        cur.next = new ListNode(key);
     }
-    
+
     public void remove(int key) {
-        set.remove(key);
+        ListNode cur = set[key % set.length];
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                cur.next = cur.next.next; //delete the key 
+                return;
+            }
+            cur = cur.next;
+        }
     }
-    
+
     public boolean contains(int key) {
-        return set.contains(key);
+        ListNode cur = set[key % set.length];
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
     }
 }
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
- */
