42faf5a1727972e3cb1bd816d2dbb5171cd1da61
Mouni1205 <65585689+Mouni1205@users.noreply.github.com>
2025-07-31 19:38:40 -0500

    Time: 2 ms (92.03%), Space: 41.3 MB (65.7%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,39 +1,39 @@
-class Pair{
+/*class Pair{
     int num;
     int freq;
     Pair(int num,int freq){
         this.num = num;
         this.freq = freq;
     }
-}
+}*/
 class Solution {
-    public ArrayList<Pair> getFreqPair(String str){
+    public String getString(String str){
         int count = 1;
-        ArrayList<Pair> list = new ArrayList<>();
+        StringBuilder result = new StringBuilder();
         for(int i=0;i<str.length();i++){
             if(i+1<str.length() && str.charAt(i)==str.charAt(i+1)){
                 count++;
                 continue;
             }
             else{
-            list.add(new Pair(str.charAt(i)-'0',count));
+            result.append(count).append(str.charAt(i)-'0');
             count=1;
             }
         }
-        return list;
+        return result.toString();
     }
-    public String getString(ArrayList<Pair> list){
+    /*public String getString(ArrayList<Pair> list){
         StringBuilder str = new StringBuilder();
         for(Pair freq:list){
             str.append(freq.freq);
             str.append(freq.num);
         }
         return str.toString();
-    }
+    }*/
     public String getElement(int n,String result){
         if(n==1) return result;
-        ArrayList<Pair> list = getFreqPair(result);
-        String str = getString(list);
+        //ArrayList<Pair> list = getFreqPair(result);
+        String str = getString(result);
         return getElement(n-1,str);
     }
     public String countAndSay(int n) {
