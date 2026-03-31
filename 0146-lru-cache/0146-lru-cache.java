8943a04197e091a84101e116bd0d54e684196004
Mouni1205 <65585689+Mouni1205@users.noreply.github.com>
2026-03-31 14:01:06 -0500

    Time: 751 ms (5%), Space: 118.2 MB (100%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,73 +1,38 @@
-class ListNode{
-    int key;
-    int value;
-    ListNode prev=null;
-    ListNode next=null;
-    ListNode(int key,int value){
-        this.key = key;
-        this.value = value;
-    }
-}
-class LRUCache {
-    HashMap<Integer,ListNode> map;
-    int capacity;
-    ListNode dummyHead;
-    ListNode dummyTail;
+public class LRUCache {
+
+    private ArrayList<int[]> cache;
+    private int capacity;
 
     public LRUCache(int capacity) {
+        this.cache = new ArrayList<>();
         this.capacity = capacity;
-        map=new HashMap<>();
-        dummyHead = new ListNode(-1,-1);
-        dummyTail = new ListNode(-1,-1);
-        dummyHead.next=dummyTail;
-        dummyTail.prev=dummyHead;
     }
-    
+
     public int get(int key) {
-        if(!map.containsKey(key)) return -1;
-        ListNode crnt = map.get(key);
-        remove(crnt);
-        add(crnt);
-        return crnt.value;
+        for (int i = 0; i < cache.size(); i++) {
+            if (cache.get(i)[0] == key) {
+                int[] tmp = cache.remove(i);
+                cache.add(tmp);
+                return tmp[1];
+            }
+        }
+        return -1;
     }
-    
+
     public void put(int key, int value) {
-        if(map.containsKey(key)){
-            ListNode crnt = map.get(key);
-            crnt.value = value;
-            remove(crnt);
-            add(crnt);
-        }else{
-        if(map.size()==capacity){
-            ListNode lru = dummyTail.prev;
-            map.remove(lru.key);
-            lru.prev.next=dummyTail;
-            dummyTail.prev = lru.prev;
-            lru.next = null;
-            lru.prev = null;
+        for (int i = 0; i < cache.size(); i++) {
+            if (cache.get(i)[0] == key) {
+                int[] tmp = cache.remove(i);
+                tmp[1] = value;
+                cache.add(tmp);
+                return;
+            }
         }
-            ListNode crnt = new ListNode(key,value);
-            map.put(key,crnt);
-            add(crnt);
+
+        if (capacity == cache.size()) {
+            cache.remove(0);
         }
-    }
-    public void add(ListNode node){
-        dummyHead.next.prev = node;
-        node.next = dummyHead.next;
-        dummyHead.next = node;
-        node.prev = dummyHead;
-    }
-    public void remove(ListNode node){
-        node.prev.next = node.next;
-        node.next.prev=node.prev;
-        node.prev = null;
-        node.next = null;
+
+        cache.add(new int[]{key, value});
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
