7dd7cdd781b2184abd07d2ff4d8d9e652d31d906
Mouni1205 <65585689+Mouni1205@users.noreply.github.com>
2026-04-02 18:42:51 -0500

    Time: 5 ms (44.24%), Space: 49.9 MB (28.66%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -6,14 +6,17 @@ class Solution {
 }
 
 private void restoreIp(String ip, List<String> solutions, int idx, String restored, int count) {
-    if (count > 4) return;
-    if (count == 4 && idx == ip.length()) solutions.add(restored);
+    if (count == 4) {
+        if (idx == ip.length()) solutions.add(restored); // Add valid IP
+        return; // Stop recursion when count is 4
+    }
     
-    for (int i=1; i<4; i++) {
-        if (idx+i > ip.length()) break;
-        String s = ip.substring(idx,idx+i);
-        if ((s.startsWith("0") && s.length()>1) || (i==3 && Integer.parseInt(s) >= 256)) continue;
-        restoreIp(ip, solutions, idx+i, restored+s+(count==3?"" : "."), count+1);
+    for (int i = 1; i < 4; i++) {
+        if (idx + i > ip.length()) break;
+        String s = ip.substring(idx, idx + i);
+        if ((s.startsWith("0") && s.length() > 1) || (i == 3 && Integer.parseInt(s) >= 256)) continue;
+        restoreIp(ip, solutions, idx + i, restored + s + (count == 3 ? "" : "."), count + 1);
     }
-    }
+}
+    
 }
