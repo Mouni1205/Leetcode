8e889d5107fa8c8c6db0e9cb6447eb5cae4aa526
Mouni1205 <65585689+Mouni1205@users.noreply.github.com>
2026-04-02 18:54:04 -0500

    Time: 6 ms (35.97%), Space: 50 MB (24.44%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -13,8 +13,11 @@ private void restoreIp(String ip, List<String> solutions, int idx, String restor
     
     for (int i = 1; i < 4; i++) {
         if (idx + i > ip.length()) break;
+        //decides where you want to put .
         String s = ip.substring(idx, idx + i);
+        //check if where you are putting . after a string, would that string start with 0 or >256 then abort
         if ((s.startsWith("0") && s.length() > 1) || (i == 3 && Integer.parseInt(s) >= 256)) continue;
+        //add that to the current string and add .  and increase count 
         restoreIp(ip, solutions, idx + i, restored + s + (count == 3 ? "" : "."), count + 1);
     }
 }
