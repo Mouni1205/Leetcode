1bad36c99f35643d339d2e01f38574d1a42de672
Mouni1205 <65585689+Mouni1205@users.noreply.github.com>
2026-04-02 19:59:22 -0500

    Time: 1 ms (84.22%), Space: 42.8 MB (79.13%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,28 +1,27 @@
+
 class Solution {
     public String decodeString(String s) {
-        Stack<Integer> stack = new Stack<>();
-        Stack<StringBuilder> stackSB = new Stack<>();
-        StringBuilder str = new StringBuilder();
-        int number = 0;
+        Stack<Pair<StringBuilder, Integer>> stack = new Stack<>();
+        StringBuilder currentStr = new StringBuilder();
+        int num = 0;
 
         for (char c : s.toCharArray()) {
             if (Character.isDigit(c)) {
-                number = number * 10 + (c - '0');
+                num = num * 10 + (c - '0'); // Build the number
             } else if (c == '[') {
-                stack.push(number);
-                stackSB.push(str);
-                str = new StringBuilder();
-                number = 0;
+                stack.push(new Pair<>(currentStr, num)); // Push current string and number
+                currentStr = new StringBuilder(); // Reset for new substring
+                num = 0;
             } else if (c == ']') {
-                int repeat = stack.pop();
-                StringBuilder current = stackSB.pop();
-                for (int i = 0; i < repeat; i++) current.append(str);
-                str = current;
+                Pair<StringBuilder, Integer> p = stack.pop(); // Pop from stack
+                StringBuilder prevStr = p.getKey();
+                int repeat = p.getValue();
+                for (int i = 0; i < repeat; i++) prevStr.append(currentStr); // Repeat and append
+                currentStr = prevStr; // Update current string
             } else {
-                str.append(c);
+                currentStr.append(c); // Build current string
             }
         }
-
-        return str.toString();
+        return currentStr.toString();
     }
 }
