d887d4727c708828460e3c1105ab3c5202fc4aec
Mouni1205 <65585689+Mouni1205@users.noreply.github.com>
2026-06-06 13:10:07 -0500

    Time: 0 ms (100%), Space: 42.7 MB (72.48%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
         if(s.length()!=goal.length()) return false;
+        //When you double the string, you're creating a "window" where every possible split point becomes visible as a contiguous substring.
         s+=s;
         return s.contains(goal);
     }
